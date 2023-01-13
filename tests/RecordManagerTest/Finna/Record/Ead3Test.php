e5f9e7fba509fb4ac5858018a6000a243ff8864d
rajaro <jaro.ravila@gmail.com>
2023-01-13 15:54:23 +0200

    Use data provider and only 1 fixture file

--- tests/RecordManagerTest/Finna/Record/Ead3Test.php
+++ tests/RecordManagerTest/Finna/Record/Ead3Test.php
@@ -234,71 +234,87 @@ class Ead3Test extends \RecordManagerTest\Base\Record\RecordTest
     }
 
     /**
-     * Test AHAA EAD3 record handling
+     * Helper function for getTestTitleYearRange
+     *
+     * @param string $newTitle new title for test case
      *
      * @return void
      */
-    public function testAhaa14()
+    public function modifyAhaa14Fixture($newTitle)
     {
-        // Test year range with ndash
-        $fields = $this->createRecord(Ead3::class, 'ahaa14.xml', [], 'Finna')
-            ->toSolrArray();
+        $fixture = $this->getFixture('record/ahaa14.xml', 'Finna');
+        $fixturePath = $this->getFixturePath('record/ahaa14.xml', 'Finna');
+        $titleReg = '/>(.*)<\/unittitle>/';
+        $title = ">" . $newTitle . "</unittitle>";
+        $fixture = preg_replace(
+            $titleReg,
+            $title,
+            $fixture
+        );
+        file_put_contents($fixturePath, $fixture);
+    }
+
+    /**
+     * Data provider for testTitleYearRange
+     *
+     * @return Generator
+     */
+    public function getTestTitleYearRange()
+    {
         $ndash = html_entity_decode('&#x2013;', ENT_NOQUOTES, 'UTF-8');
-        $this->assertEquals(
-            "Opintokirja. Helsingin yliopisto (1932{$ndash}1935)",
-            $fields['title']
-        );
-    }
-
-    /**
-     * Test AHAA EAD3 record handling
-     *
-     * @return void
-     */
-    public function testAhaa15()
-    {
-        // Test year range with hyphen
-        $fields = $this->createRecord(Ead3::class, 'ahaa15.xml', [], 'Finna')
-            ->toSolrArray();
-        $this->assertEquals(
-            "Opintokirja. Helsingin yliopisto 1932 - 1935",
-            $fields['title']
-        );
-    }
-
-    /**
-     * Test AHAA EAD3 record handling
-     *
-     * @return void
-     */
-    public function testAhaa16()
-    {
-        // Test year range with mdash
-        $fields = $this->createRecord(Ead3::class, 'ahaa16.xml', [], 'Finna')
-            ->toSolrArray();
         $mdash = html_entity_decode('&#x2014;', ENT_NOQUOTES, 'UTF-8');
-        $this->assertEquals(
+        $this->modifyAhaa14Fixture(
+            "Opintokirja. Helsingin yliopisto (1932{$ndash}1935)"
+        );
+        $record = $this->createRecord(Ead3::class, 'ahaa14.xml', [], 'Finna')
+            ->toSolrArray();
+        yield 'test ndash' => [
+            "Opintokirja. Helsingin yliopisto (1932{$ndash}1935)",
+            $record['title']
+        ];
+
+        $this->modifyAhaa14Fixture(
+            "Opintokirja. Helsingin yliopisto 1932{$mdash}1935"
+        );
+        $record = $this->createRecord(Ead3::class, 'ahaa14.xml', [], 'Finna')
+            ->toSolrArray();
+        yield 'test mdash' => [
             "Opintokirja. Helsingin yliopisto 1932{$mdash}1935",
-            $fields['title']
+            $record['title']
+        ];
+
+        $this->modifyAhaa14Fixture(
+            "Opintokirja. Helsingin yliopisto (1932 - 1935)"
         );
+        $record = $this->createRecord(Ead3::class, 'ahaa14.xml', [], 'Finna')
+            ->toSolrArray();
+        yield 'test dash' => [
+            "Opintokirja. Helsingin yliopisto (1932 - 1935)",
+            $record['title']
+        ];
+
+        $this->modifyAhaa14Fixture("Opintokirja. Helsingin yliopisto");
+        $record = $this->createRecord(Ead3::class, 'ahaa14.xml', [], 'Finna')
+            ->toSolrArray();
+        yield 'test without year range' => [
+            "Opintokirja. Helsingin yliopisto (1932{$ndash}1935)",
+            $record['title']
+        ];
     }
 
     /**
-     * Test AHAA EAD3 record handling
+     * Test AHAA EAD3 title year range handling
+     *
+     * @param array $expected Expected results
+     * @param array $input    Input
+     *
+     * @dataProvider getTestTitleYearRange
      *
      * @return void
      */
-    public function testAhaa17()
+    public function testTitleYearRange($expected, $input)
     {
-        // Test adding year range to title
-        $fields = $this->createRecord(Ead3::class, 'ahaa17.xml', [], 'Finna')
-            ->toSolrArray();
-        $mdash = html_entity_decode('&#x2014;', ENT_NOQUOTES, 'UTF-8');
-        $ndash = html_entity_decode('&#x2013;', ENT_NOQUOTES, 'UTF-8');
-        $this->assertEquals(
-            "Opintokirja. Helsingin yliopisto (1932{$ndash}1935)",
-            $fields['title']
-        );
+        $this->assertEquals($expected, $input);
     }
 
     /**
